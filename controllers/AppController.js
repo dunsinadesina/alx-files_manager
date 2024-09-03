10867c3e886be470ad383c36434fbef42c140a64
dunsinadesina <adesinajesudunsin@gmail.com>
2024-09-03 12:59:16 +0100

    updated work

---
+++ controllers/AppController.js
@@ -0,0 +1,16 @@
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+class AppController {
+  static getStatus(request, response) {
+    response.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
+  }
+
+  static async getStats(request, response) {
+    const usersNum = await dbClient.nbUsers();
+    const filesNum = await dbClient.nbFiles();
+    response.status(200).json({ users: usersNum, files: filesNum });
+  }
+}
+
+module.exports = AppController;
